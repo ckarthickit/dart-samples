12f755037893ae4e47291a0f5d7c323e76a3d170
CHINNATHAMBI, KARTHICK (kc3503) <kc3503@intl.att.com>
2019-01-26 16:22:04 +0530

    Contacting api.github

--- 03_library_package_creation/01_app_package_demo/bin/app_demo.dart
+++ 03_library_package_creation/01_app_package_demo/bin/app_demo.dart
@@ -3,12 +3,12 @@ import 'package:http/http.dart' as http;
 
 void main() {
 	print("Application package demo");
-	var url = "http://example.com/whatsit/create";
+	var url = "https://api.github.com";
 	http.post(url, body: {"name": "doodle", "color": "blue"})
     .then((response) {
   		print("Response status: ${response.statusCode}");
   		print("Response body: ${response.body}");
 	});
 
-	//http.read("http://example.com/foobar.txt").then(print);
+	http.read("https://api.github.com").then(print);
 }
